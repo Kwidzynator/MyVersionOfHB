719f5f7caf8c78c1e1b1d434fecb99ce07f238db
Natan <natanwarelich@mat.umk.pl>
2024-09-15 20:43:28 +0200

    done

--- src/Controller/WordsController.php
+++ src/Controller/WordsController.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class WordsController extends AbstractController
+{
+    #[Route('/words', name: 'app_words')]
+    public function index(): Response
+    {
+
+
+        return $this->render('games/words.html.twig', [
+            'controller_name' => 'WordsController',
+        ]);
+    }
+}
